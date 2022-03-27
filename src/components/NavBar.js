cebad454c8d6562b4587c766ea5de4c07b4b450b
Samsudin Mohamad <sam88mohd@gmail.com>
2022-03-27 15:41:09 +0800

    do housekeeping

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
 const NavBar = () => {
